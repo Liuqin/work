4883aa8e8df5ff0c8c032ff747d442ef6a502023
Liuqin <sata123456!>
2020-07-03 10:24:54 +0800

    init porject

---
+++ src/test/java/com/qin/work/WorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qin.work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
