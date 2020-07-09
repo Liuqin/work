6bc9d012dee23f351ce34bdae8c1019a113557a2
Liuqin <sata123456!>
2020-07-09 10:36:26 +0800

    新增zk 集群，新增ui

--- zkcluster.sh
+++ zkcluster.sh
@@ -2,7 +2,7 @@
 cd zk
 echo 'zk 集群启动本地的 2181, 2182, 2183 端口 '
 #docker pull zookeeper
-docker-compose stop zk_cluster
+docker-compose down -v
 COMPOSE_PROJECT_NAME=zk_cluster docker-compose up -d
 
 read -p "服务器地址,默认127.0.0.1:" local_ip
@@ -15,6 +15,7 @@ if [ ! -n "$local_ip" ]; then
 else
   echo $local_ip
 fi
+docker rm zkui
 docker run -d --name zkui -p 9090:9090 -e ZKUI_ZK_SERVER=$(echo $local_ip):2181 qnib/zkui
 echo 'zui 启动在9090 端口'
 cd /work
