5b33cba1e1ff024fb14ca506e4b350c31c5556fb
liuqin <287428729@qq.com>
2020-07-18 17:46:44 +0800

    新增 gitea 代码托管

--- gitea.sh
+++ gitea.sh
@@ -8,3 +8,4 @@ sudo rm -rf /var/lib/gitea
 sudo mkdir -p /var/lib/gitea
 docker rm -f liuqin-gitea
 docker run -itd --privileged=true  --restart=always  --name=liuqin-gitea -p 10022:22 -p 6001:3000 -v /var/lib/gitea:/data gitea/gitea:latest
+echo '在6001 搭建了快速高性能代码仓库'
