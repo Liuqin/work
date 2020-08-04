d3651d474db387d3118bac16e631ce31de527b5a
liuqin <287428729@qq.com>
2020-08-04 10:54:47 +0800

    新增k3s

--- k3s.sh
+++ k3s.sh
@@ -8,4 +8,4 @@ echo '测试获取的token: '$token
 echo '确认节点信息:'
 kubectl get node -o wide
 echo '安装worker节点:'
-curl -sfL https://docs.rancher.cn/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn K3S_URL=https://myserver:6443 K3S_TOKEN=$(echo $token)  INSTALL_K3S_EXEC="--docker" sh -
+curl -sfL https://docs.rancher.cn/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn K3S_URL=https://localhost:6443 K3S_TOKEN=$(echo $token)  INSTALL_K3S_EXEC="--docker" sh -
